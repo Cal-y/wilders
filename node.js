3ee620334b2a3a55460fa65a2b682eb928c900f1
Calypso salé <calypso45000@gmail.com>
2023-10-03 11:08:14 +0200

    Initial commit

---
+++ node.js
@@ -0,0 +1,17 @@
+import chalk from "chalk";
+
+console.log(
+    chalk.blue("Maxence")
+);
+
+console.log(
+    chalk.yellow("Calypso")
+);
+
+console.log(
+    chalk.green("Alexis")
+);
+
+console.log(
+    chalk.red("Jean-Louis")
+);
